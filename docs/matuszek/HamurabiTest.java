75366f0cb4c142dd43287f669d98e6290e0baa01
dave <davejourdain@gmail.com>
2023-07-30 19:30:47 -0400

    still working

---
+++ docs/matuszek/HamurabiTest.java
@@ -0,0 +1,13 @@
+
+package hammurabi.src.test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.Hammurabi;
+
+public class HammurabiTest {
+
+    Hammurabi ham;
